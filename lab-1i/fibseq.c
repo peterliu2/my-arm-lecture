2440159c9d45528b28bc81568ea2c001b911cb42
peterliu2 <peter.liu.work@gmail.com>
2016-01-14 20:21:31 +0800

    astyle it

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -4,30 +4,30 @@ int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+    int number=0;
+    int result=0;
 
-  (void) scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+    (void) scanf("%d",&number);
+    result = fibonacci(number);
+    printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
 
 int fibonacci(int x)
 {
-	int nowValue = 0;
-	int preValue_1 = 1;
-	int preValue_2 = 0;
+    int nowValue = 0;
+    int preValue_1 = 1;
+    int preValue_2 = 0;
 
-	if (x == 0) return 0;
-	if (x == 1) return 1;
+    if (x == 0) return 0;
+    if (x == 1) return 1;
 
-	for (int i=0; i<= x-2; i++)
-	{
-		nowValue   = preValue_1 + preValue_2;
-		preValue_2 = preValue_1;
-		preValue_1 = nowValue;
-	}
+    for (int i=0; i<= x-2; i++)
+    {
+        nowValue   = preValue_1 + preValue_2;
+        preValue_2 = preValue_1;
+        preValue_1 = nowValue;
+    }
 
-	return nowValue;
+    return nowValue;
 }
 
