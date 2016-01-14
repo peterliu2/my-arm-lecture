d78a0752623c6271050e48bff32f32b2f0585060
peterliu2 <peter.liu.work@gmail.com>
2016-01-14 20:19:19 +0800

    add iteration code

--- lab-1i/fibseq.c
+++ lab-1i/fibseq.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
-extern int fibonacci(int x);
+int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
   int number=0;
   int result=0;
 
-  scanf("%d",&number);
+  (void) scanf("%d",&number);
   result = fibonacci(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+}
+
+int fibonacci(int x)
+{
+	int nowValue = 0;
+	int preValue_1 = 1;
+	int preValue_2 = 0;
+
+	if (x == 0) return 0;
+	if (x == 1) return 1;
+
+	for (int i=0; i<= x-2; i++)
+	{
+		nowValue   = preValue_1 + preValue_2;
+		preValue_2 = preValue_1;
+		preValue_1 = nowValue;
+	}
+
+	return nowValue;
 }
 
